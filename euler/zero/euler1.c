e4de345d0865b8e3ea875ce289244a1629e6ccea
J. Anthony Sterrett, Jr <jasterrett@gmail.com>
2015-05-01 17:38:40 -0700

    Fixed euler1.c
    Stupid mistake... I was compiling and running one/euler1.c, not
    zero/euler1.c. The code in zero/euler1.c was therefore completely
    invalid. Oops.

--- euler/zero/euler1.c
+++ euler/zero/euler1.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {if (int sum=0,int i=0)else while(i<1000)if(i%3&&i%5&&sum+=i++)else if(i++){}if(printf("%d\n",i)&&exit(0))}
+int main(int i, char* argv[]) {
+
+  if (argv=malloc(sizeof(int)),*(int*)(argv)=0,i=0){}else while(i<1000)if(i%3&&i%5&&(*(int*)(argv)+=i++)){}else if(i++){}if(printf("%d\n",*(int*)(argv))&&(exit(0),0)){}}
